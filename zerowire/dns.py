9b23b4e10b431b4b4df0c3622666da378a90bee2
Greg "GothAck" Miell <greg@gothack.ninja>
2019-12-22 13:31:39 +0000

    Split DNS server into base class etc

--- zerowire/dns.py
+++ zerowire/dns.py
@@ -10,6 +10,7 @@ from typing import (
 import asyncio
 import logging
 import ipaddress
+from abc import ABC, abstractmethod
 
 import dbus
 from dnslib import DNSRecord, DNSHeader, DNSLabel, RR
@@ -19,7 +20,39 @@ if TYPE_CHECKING:
 
 logger = logging.getLogger(__name__)
 
-class SimpleDNSServer:
+
+class DNSDatagramProtocol(asyncio.DatagramProtocol):
+    transport: asyncio.DatagramTransport
+
+    def __init__(self, server: BaseDNSServer) -> None:
+        self.server = server
+        super().__init__()
+
+    def connection_made(self, transport: asyncio.BaseTransport) -> None:
+        self.transport = cast(asyncio.DatagramTransport, transport)
+
+    def datagram_received(self, data: Union[bytes, str], addr: Tuple[str, int]) -> None:
+        reply = self.server.handle_query(DNSRecord.parse(data))
+        self.transport.sendto(reply.pack(), addr)
+
+
+class BaseDNSServer:
+    def __init__(self, bind: str, port: int):
+        self.bind = ipaddress.ip_address(bind)
+        self.port = port
+        self.loop = asyncio.get_event_loop()
+
+    async def start(self) -> Tuple[Any, Any]:
+        return await self.loop.create_datagram_endpoint(
+            lambda: DNSDatagramProtocol(self),
+            local_addr=(self.bind.compressed, self.port))
+
+    @abstractmethod
+    def handle_query(self, request: DNSRecord) -> DNSRecord:
+        pass
+
+
+class SimpleDNSServer(BaseDNSServer):
     records: Dict[DNSLabel, str]
 
     def __init__(self, bind: str, port: int):
@@ -39,6 +72,17 @@ class SimpleDNSServer:
     def rem_record(self, name: str) -> None:
         del self.records[name]
 
+    def handle_query(self, request: DNSRecord) -> DNSRecord:
+        reply = request.reply()
+
+        for question in request.questions:
+            logger.debug('Question %r', question.qname)
+            record = self.records.get(question.qname)
+            if record is not None:
+                reply.add_answer(*RR.fromZone(record))
+
+        return reply
+
     def add_to_resolved(self, iface: WGInterface) -> None:
         logger.debug('ifindex %r', iface.wg_ifindex)
         bus = dbus.SystemBus()
@@ -47,26 +91,3 @@ class SimpleDNSServer:
         dbus_iface = dbus.Interface(dbus_proxy, 'org.freedesktop.resolve1.Manager')
         dbus_iface.SetLinkDNS(iface.wg_ifindex, [(2, [int(b) for b in self.bind.packed])])
         dbus_iface.SetLinkDomains(iface.wg_ifindex, [['zerowire.', True]])
-
-
-class DNSDatagramProtocol(asyncio.DatagramProtocol):
-    transport: asyncio.DatagramTransport
-
-    def __init__(self, server: SimpleDNSServer) -> None:
-        self.server = server
-        super().__init__()
-
-    def connection_made(self, transport: asyncio.BaseTransport) -> None:
-        self.transport = cast(asyncio.DatagramTransport, transport)
-
-    def datagram_received(self, data: Union[bytes, str], addr: Tuple[str, int]) -> None:
-        request = DNSRecord.parse(data)
-        reply = request.reply()
-
-        for question in request.questions:
-            logger.debug('Question %r', question.qname)
-            record = self.server.records.get(question.qname)
-            if record is not None:
-                reply.add_answer(*RR.fromZone(record))
-
-        self.transport.sendto(reply.pack(), addr)
